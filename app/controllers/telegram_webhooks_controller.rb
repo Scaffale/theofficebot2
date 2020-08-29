dfea3234bb96b702265abd52b0df62f9c9e6a715
Martino <martino.giacchetti@gmail.com>
2020-08-29 10:54:40 +0200

    added logger, fixed ffmpeg

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -1,9 +1,9 @@
 # Main class, manages inline queries
 class TelegramWebhooksController < Telegram::Bot::UpdatesController
   def inline_query(query, offset)
-    p 'INIZIO IL METODO'
-    p query
-    p offset
+    Rails.logger.info 'INIZIO IL METODO'
+    Rails.logger.info query
+    Rails.logger.info offset
     results_query = Sentence.where('LOWER(text) LIKE LOWER(?)', "%#{query}%").limit(10)
     results_query.map(&:build_gif)
     results = results_query.map do |result|
@@ -14,6 +14,7 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
         thumb_url: [ENV['SERVER_URL'], 'placeholder.jpg'].join('/')
       }
     end
+    Rails.logger.info results
     answer_inline_query results
   end
 end
--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,16 +1,13 @@
 # Sentence model manages to create gif
 class Sentence < ApplicationRecord
   def build_gif(_delta_before = 0, _delta_after = 0)
-    comand = "ffmpeg -ss #{time_start.strftime(time_to_string_ffmpeg)} -loglevel panic -n
-             -i #{Rails.root}/data/#{file_name}.mp4
-             -to #{Time.at(time_end).utc.strftime(time_to_string_ffmpeg)} -c copy
-             #{Rails.public_path}/gifs/#{new_name}.mp4"
+    comand = "ffmpeg -ss #{time_start.strftime(time_to_string_ffmpeg)} -loglevel panic -n -i #{Rails.root}/data/#{file_name}.mp4 -to #{Time.at(time_end).utc.strftime(time_to_string_ffmpeg)} -c copy #{Rails.public_path}/gifs/#{new_name}"
     # comand = "ffmpeg -ss #{time_start.strftime(time_to_string_ffmpeg)}
     # -loglevel panic -n -i #{Rails.root}/data/#{file_name}.mp4
     # -to #{Time.at(time_end).utc.strftime(time_to_string_ffmpeg)} -c copy
     # -avoid_negative_ts 1 #{Rails.public_path}/gifs/#{new_name}.mp4"
     system(comand)
-    "/gifs/#{new_name}.mp4"
+    "/gifs/#{new_name}"
   end
 
   def time_to_string
@@ -23,7 +20,7 @@ class Sentence < ApplicationRecord
 
   def new_name(delta_before = 0, delta_after = 0)
     [file_name, time_start(delta_before).strftime(time_to_string),
-     Time.at(time_end(delta_after)).utc.strftime(time_to_string)].join('-')
+     Time.at(time_end(delta_after)).utc.strftime(time_to_string), '.mp4'].join('-')
   end
 
   def time_start(delta_before = 0)
