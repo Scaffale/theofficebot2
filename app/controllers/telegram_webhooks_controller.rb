36af7f6aa9cced6567fed4d449c5e178a2de33a1
Martino <martino.giacchetti@gmail.com>
2020-08-29 10:43:58 +0200

    forced creation of gif before answering

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -1,14 +1,16 @@
 # Main class, manages inline queries
 class TelegramWebhooksController < Telegram::Bot::UpdatesController
   def inline_query(query, offset)
+    p 'INIZIO IL METODO'
     p query
     p offset
     results_query = Sentence.where('LOWER(text) LIKE LOWER(?)', "%#{query}%").limit(10)
+    results_query.map(&:build_gif)
     results = results_query.map do |result|
       {
         type: 'mpeg4_gif',
         id: result.new_name,
-        mpeg4_url: [ENV['SERVER_URL'], result.build_gif].join('/'),
+        mpeg4_url: [ENV['SERVER_URL'], 'gifs', result.new_name].join('/'),
         thumb_url: [ENV['SERVER_URL'], 'placeholder.jpg'].join('/')
       }
     end
